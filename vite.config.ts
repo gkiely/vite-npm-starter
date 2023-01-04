487ded840ca2b9fd5ff7497a111bea19af7816bd
Grant Kiely <grant.kiely@gmail.com>
2023-01-04 11:09:19 -0600

    updated working version

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,23 @@
 import { defineConfig } from 'vitest/config';
 import checker from 'vite-plugin-checker';
-import clearVitest from './scripts/vite-plugin-clear-vitest';
+
 import typescript from '@rollup/plugin-typescript';
 import path from 'path';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import license from 'rollup-plugin-license';
-const TEST = typeof process !== 'undefined' && process.env.NODE_ENV === 'test';
-const DEV = !TEST;
-const event = process.env.npm_lifecycle_event;
-const WATCH_TEST = TEST && event === 'test';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  // When generating types, run in node environment
-  test: {
-    setupFiles: ['./setup.vitest.ts'],
-    environment: 'node',
-    globals: true,
-    css: false,
-    isolate: false,
-    passWithNoTests: true,
-    coverage: {
-      enabled: false,
-    },
-  },
   plugins: [
-    DEV &&
-      checker({
-        typescript: true,
-        eslint: {
-          lintCommand: 'eslint -c .eslintrc.json --cache --fix --ext ts,tsx src',
-          dev: {
-            logLevel: ['error'],
-          },
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint -c .eslintrc.json --cache --fix --ext ts,tsx src',
+        dev: {
+          logLevel: ['error'],
         },
-      }),
-    // Clear terminal plugin for vitest
-    WATCH_TEST && clearVitest(),
+      },
+    }),
     license({
       thirdParty: {
         output: path.resolve(__dirname, './dist/assets/vendor.LICENSE.txt'),
@@ -70,7 +51,7 @@ export default defineConfig(() => ({
           sourceMap: false,
           declaration: true,
           outDir: 'dist',
-          exclude: ['**/*.test.ts', 'tsconfig.tsbuildinfo'],
+          exclude: ['**/*.test.ts', 'vitest.config.ts', 'tsconfig.tsbuildinfo'],
         }),
       ],
     },
